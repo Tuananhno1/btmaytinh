171dee9b16dcde4f49459d5a09f76ff6366a8783
Tuananhno1 <anhntd14ptit@gmail.com>
2019-04-25 17:21:11 +0700

    btmaytinh

---
+++ btmaytinh.js
@@ -0,0 +1,28 @@
+function maytinh1() {
+    let dauvao1 = parseFloat(document.getElementById('a0').value);
+    let dauvao2 = parseFloat(document.getElementById('a1').value);
+    let ketqua;
+    ketqua = dauvao1 + dauvao2;
+    document.getElementById('ketqua').value = ketqua;
+}
+function maytinh2() {
+    let dauvao1 = parseFloat(document.getElementById('a0').value);
+    let dauvao2 = parseFloat(document.getElementById('a1').value);
+    let ketqua;
+    ketqua = dauvao1 - dauvao2;
+    document.getElementById('ketqua').value = ketqua;
+}
+function maytinh3() {
+    let dauvao1 = parseFloat(document.getElementById('a0').value);
+    let dauvao2 = parseFloat(document.getElementById('a1').value);
+    let ketqua;
+    ketqua = dauvao1 * dauvao2;
+    document.getElementById('ketqua').value = ketqua;
+}
+function maytinh4() {
+    let dauvao1 = parseFloat(document.getElementById('a0').value);
+    let dauvao2 = parseFloat(document.getElementById('a1').value);
+    let ketqua;
+    ketqua = dauvao1 / dauvao2;
+    document.getElementById('ketqua').value = ketqua;
+}
